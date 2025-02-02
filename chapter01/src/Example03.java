ae218b20902dcd09c621caa83dba3b762a8031c7
Wilmer Tezen <appserviciosgt@outlook.com>
2025-02-02 17:41:13 -0600

    repo updated

---
+++ chapter01/src/Example03.java
@@ -0,0 +1,23 @@
+public class Example03 {
+    public static void main(String[] args) {
+        String t = "This is a text";
+        System.out.println(t);
+        System.out.println("A");
+        System.out.println(53);
+        System.out.println(23.08f);
+        System.out.println(1.97);
+        System.out.println(true);
+
+        char c = 'A';
+        int i = 53;
+        float f = 53.08f;
+        double d = 1.97;
+        boolean b = true;
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+    }
+
+}
---
+++ chapter01/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        System.out.println("This is text");
+        System.out.println('A');
+        System.out.println(53);
+        System.out.println(23.08f);
+        System.out.println(1.97);
+        System.out.println(true);
+
+    }
+}
