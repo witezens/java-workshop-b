0c3d63be0ca103e64d486c4814727161d2dcd8b3
Wilmer Tezen <appserviciosgt@outlook.com>
2025-02-02 16:09:29 -0600

    initial commit

---
+++ chapter01/Example04.java
@@ -0,0 +1,11 @@
+public class Example04 {
+    public static void main(String[] args) {
+
+        String t = null;
+        System.out.println(t);
+        t = "Joe ...";
+        System.out.println(t);
+        t = "went finishing";
+        System.out.println(t);
+    }
+}
